de0cbb89771a1fea94fe53a56bccff7f03660805
Thomas Yeung <yeung.thomas.h@gmail.com>
2024-11-09 13:20:03 -0800

    added Newsflash heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import "./App.css";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PreferencesPage from "./pages/preferences";
 import SummaryPage from "./pages/summary";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PreferencesPage />} />
-        <Route path="/summary" element={<SummaryPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Newsflash</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PreferencesPage />} />
+          <Route path="/summary" element={<SummaryPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
