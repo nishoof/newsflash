4a01920f8bc58193d04d0e466286bcf3a9465a75
nishoof <88177986+nishoof@users.noreply.github.com>
2024-11-09 01:24:32 -0800

    More tweaking

--- src/Summarizer/summarizer.tsx
+++ src/Summarizer/summarizer.tsx
@@ -1,6 +1,6 @@
 import ollama from 'ollama';
 
-const systemPrompt = 'Your job is simply to summarize the content from the articles below. Please keep your response to around 300 words overall. Do not specify which article the information is from. Just read all of the articles I provide and paraphrase all of the information. For example, if there are 3 articles talking about a new game being released, just summarize in one paragraph the key points from the 3 articles. Here are the articles: ';
+const systemPrompt = 'Your job is simply to summarize the content from the articles below. Please keep your response to around 300 words overall. Do not specify which article the information is from. Just read all of the articles I provide and paraphrase all of the information. For example, if there are 3 articles talking about a new game being released, just summarize in one paragraph the key points from the 3 articles. Do not specify the articles you are getting the information from. Just give a paragraph. Here are the articles: ';
 
 export async function summarizeArticle(articles: string) {
     const prompt = systemPrompt + '\n\n\n' + articles;
@@ -10,7 +10,12 @@ export async function summarizeArticle(articles: string) {
     const response = await ollama.generate({
         model: 'llama3.2',
         prompt: prompt,
-        stream: true
+        stream: true,
+        options: {
+            repeat_penalty: 1.5,
+            top_k: 30,
+            top_p: 0.5
+        }
     });
 
     return response;
