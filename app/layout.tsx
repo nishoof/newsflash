16229112de9e5aba6b42078f49e8b2c99cc63659
Thomas Yeung <yeung.thomas.h@gmail.com>
2024-11-09 19:05:14 -0800

    css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,30 @@
-import { type Metadata } from "next"
+import { type Metadata } from "next";
+import { Comfortaa } from "next/font/google";
+
+import "../app/App.css";
+
+const comfortaa = Comfortaa({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-comfortaa",
+});
 
 export const metadata: Metadata = {
-    title: 'React App',
-    description: 'Web site created with Next.js.',
-}
+  title: "React App",
+  description: "Web site created with Next.js.",
+};
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <>
-            <html lang="en">
-                <body>
-                    <h1>Newsflash</h1>
-                    {children}
-                </body>
-            </html>
-        </>
-    )
+  return (
+    <html lang="en" className={comfortaa.variable}>
+      <body>
+        <h1>Newsflash</h1>
+        {children}
+      </body>
+    </html>
+  );
 }
